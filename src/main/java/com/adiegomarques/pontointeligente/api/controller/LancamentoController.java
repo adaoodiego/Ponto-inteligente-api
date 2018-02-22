748ca50a5a9b5d176e3ecf6499c5e3f4be03df5a
thema <thema@pc26.thema.inf.br>
2018-02-22 13:22:08 -0300

    adicionado ehcache

--- src/main/java/com/adiegomarques/pontointeligente/api/controller/LancamentoController.java
+++ src/main/java/com/adiegomarques/pontointeligente/api/controller/LancamentoController.java
@@ -55,7 +55,6 @@ public class LancamentoController {
 
 	public LancamentoController() {
 	}
-
 	/**
 	 * Retorna a listagem de lançamentos de um funcionário.
 	 * 
@@ -64,7 +63,8 @@ public class LancamentoController {
 	 */
 	@GetMapping(value = "/funcionario/{funcionarioId}")
 	public ResponseEntity<Response<Page<LancamentoDto>>> listarPorFuncionarioId(
-			@PathVariable("funcionarioId") Long funcionarioId, @RequestParam(value = "pag", defaultValue = "0") int pag,
+			@PathVariable("funcionarioId") Long funcionarioId,
+			@RequestParam(value = "pag", defaultValue = "0") int pag,
 			@RequestParam(value = "ord", defaultValue = "id") String ord,
 			@RequestParam(value = "dir", defaultValue = "DESC") String dir) {
 		log.info("Buscando lançamentos por ID do funcionário: {}, página: {}", funcionarioId, pag);
@@ -106,7 +106,7 @@ public class LancamentoController {
 	 * @param lancamento
 	 * @param result
 	 * @return ResponseEntity<Response<LancamentoDto>>
-	 * @throws ParseException
+	 * @throws ParseException 
 	 */
 	@PostMapping
 	public ResponseEntity<Response<LancamentoDto>> adicionar(@Valid @RequestBody LancamentoDto lancamentoDto,
@@ -133,7 +133,7 @@ public class LancamentoController {
 	 * @param id
 	 * @param lancamentoDto
 	 * @return ResponseEntity<Response<Lancamento>>
-	 * @throws ParseException
+	 * @throws ParseException 
 	 */
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Response<LancamentoDto>> atualizar(@PathVariable("id") Long id,
@@ -179,7 +179,8 @@ public class LancamentoController {
 	}
 
 	/**
-	 * Valida um funcionário, verificando se ele é existente e válido no sistema.
+	 * Valida um funcionário, verificando se ele é existente e válido no
+	 * sistema.
 	 * 
 	 * @param lancamentoDto
 	 * @param result
@@ -221,10 +222,9 @@ public class LancamentoController {
 	 * @param lancamentoDto
 	 * @param result
 	 * @return Lancamento
-	 * @throws ParseException
+	 * @throws ParseException 
 	 */
-	private Lancamento converterDtoParaLancamento(LancamentoDto lancamentoDto, BindingResult result)
-			throws ParseException {
+	private Lancamento converterDtoParaLancamento(LancamentoDto lancamentoDto, BindingResult result) throws ParseException {
 		Lancamento lancamento = new Lancamento();
 
 		if (lancamentoDto.getId().isPresent()) {
